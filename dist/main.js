9ce71b66d4365f38856d1298bc0cf8c2064a0a2f
Yejin Park <o0o2211915@gmail.com>
2022-11-14 09:22:20 +0900

    add config (jenkins_home and port)

--- dist/main.js
+++ dist/main.js
@@ -2,9 +2,22 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+const commander_1 = require("commander");
 async function bootstrap() {
+    const program = new commander_1.Command();
+    program.command("centralpark");
+    program.allowUnknownOption();
+    program.option("--port <port>", "set port number", "3000");
+    program.option("--jenkins_home <jenkin_home>", "set jenkins home directiory path");
+    program.parse(process.argv);
+    const options = program.opts();
+    const port = options.port;
+    const jenkins_home = options.jenkins_home;
+    console.log("testtest");
+    console.log(port);
+    console.log(jenkins_home);
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    await app.listen(3000);
+    await app.listen(port);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Command } from "commander";
 
 async function bootstrap() {
+  const program = new Command();
+  program.command("centralpark");
+  program.allowUnknownOption();
+  program.option(
+    "--port <port>",
+    "set port number",
+    "3000"
+  );
+  program.option(
+    "--jenkins_home <jenkin_home>",
+    "set jenkins home directiory path",
+  );
+  program.parse(process.argv);
+  const options = program.opts();
+  const port = options.port;
+  const jenkins_home = options.jenkins_home;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
